146cf47453a1b2660006a04d9e5709e4dd81feac
NicoFerni <nicoferni55@gmail.com>
2024-05-04 04:48:49 -0300

    typeorm Index updated

--- src/typeorm/index.ts
+++ src/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { Profile } from "./profile.entity";
 import { User } from "./user.entity";
 import { SocialNetworks } from "./socialNetworks.entity";
+import { Availability } from "./availability.entity";
 
-const entities = [User, Profile, SocialNetworks];
+const entities = [User, Profile, SocialNetworks, Availability];
 
-export {User, Profile, SocialNetworks};
+export {User, Profile, SocialNetworks, Availability};
 export default entities;
